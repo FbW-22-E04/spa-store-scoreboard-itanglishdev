dcafc63e9d4166b3eba9fd6b3e0a158033ff14a1
itanglishdev <chrishethertonweb@gmail.com>
2022-10-24 14:15:45 +0200

    all wrapped in Context

--- src/contexts/ScoreContext.js
+++ src/contexts/ScoreContext.js
@@ -2,6 +2,22 @@ import {createContext, useContext, useState} from 'react'
 
 const Context = createContext()
 
+export function ScoreContextProvider({children}) {
+
+    const [score, setScore] = useState(0)
+    
+    function plusScore(){
+        setScore(prev => prev + 10)
+    }
+
+    function minusScore(){
+        setScore(prev => prev -10)
+    }
+
+    return <Context.Provider value={{score, setScore,plusScore, minusScore}}>
+    {children}
+    </Context.Provider>
+}
 
 function ScoreContext() {
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ScoreContextProvider } from './contexts/ScoreContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ScoreContextProvider>
+        <App />
+    </ScoreContextProvider>
 );

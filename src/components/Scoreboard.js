4833b62c4edd10988ec9466b0bdf88cc5a6d564c
itanglishdev <chrishethertonweb@gmail.com>
2022-10-24 14:00:26 +0200

    added Context

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,6 +1,9 @@
+import ScoreContext from "../contexts/ScoreContext";
+
 function Scoreoard() {
     return (
         <div>
+            <ScoreContext/>
              <h2>Score</h2>
       <button type="">Add</button>
       <button type="">Subtract</button>
---
+++ src/contexts/ScoreContext.js
@@ -0,0 +1,16 @@
+import {createContext, useContext, useState} from 'react' 
+
+const Context = createContext()
+
+
+function ScoreContext() {
+
+
+    return (
+        <div>
+            
+        </div>
+      );
+}
+
+export default ScoreContext;
